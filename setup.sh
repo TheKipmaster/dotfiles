1ece678b946bda963267109e1fa15038ceebb696
Kipmaster <fkipman@hotmail.com>
2024-04-12 11:19:54 -0300

    Adds fonts and wallpapers, as well as symlinking fonts to /home/kipman

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 # 
 
 # Symlink binaries
-# [ -d $HOME/.bin ] || ln -s $HOME/dotfiles/.bin $HOME/
+# [ -d $HOME/.bin ] || ln -s $HOME/Documents/dotfiles/.bin $HOME/
 
 # Create i3 config dir if it doesn't exist already
 mkdir -p $HOME/.config/i3
 
 # Symlink i3 config files from git repo to configs
-# ln -sf $HOME/dotfiles/i3/config $HOME/.config/i3
+# ln -sf $HOME/Documents/dotfiles/i3/config $HOME/.config/i3
 
 # Symlink .profile
-# ln -sf $HOME/dotfiles/.profile $HOME
+# ln -sf $HOME/Documents/dotfiles/.profile $HOME
 
 # Symlink fonts
-# ln -sf $HOME/dotfiles/.fonts $HOME
+ln -sf $HOME/Documents/dotfiles/.fonts $HOME
 
 # Symlink dunst settings
-# ln -sf $HOME/dotfiles/dunst $HOME/.config
+# ln -sf $HOME/Documents/dotfiles/dunst $HOME/.config
