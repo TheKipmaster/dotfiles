9e686138a5184e0200e1579494395036731c9efa
TheKipmaster <kipmanf@gmail.com>
2026-04-24 08:07:03 -0300

    feat: Adds aliases to config.fish

--- config.fish
+++ config.fish
@@ -1,4 +1,22 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    starship init fish | source
+    # starship init fish | source
+    alias bat="batcat"
+    alias fd="fdfind"
+    alias pat="php artisan tinker"
+    
+    function patt
+        while true
+            set start (date +%s)
+            php artisan tinker
+            set end (date +%s)
+
+            # If the session lasted less than 2 seconds, assume it was a manual exit/CTRL+C
+            if test (math "$end - $start") -lt 2
+                break
+            end
+            
+            echo "Restarting Tinker..."
+        end
+    end
 end
